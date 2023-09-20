8f89dbed9777474455bd23b7121c45ba4a607150
User Created on 7.9 <user-created-on-7.9@sonar.org>
2023-09-20 11:31:56 +0200

    add the correct method

--- src/main/java/sonar/source/support/App.java
+++ src/main/java/sonar/source/support/App.java
@@ -1,13 +1,10 @@
 package sonar.source.support;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public void apply(@Nullable Boolean updatePermissionValue) {
+	    if (updatePermissionValue != null && updatePermissionValue) { 
+	    }
+	  }
 }
